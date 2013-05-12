054527a13f7c5796d9e6285c8a7bc9be7460639a
smismi <smirnov.ru@gmail.com>
2013-05-12 18:04:57 +0400

    node nj fix

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+(function ($) {
+
+
+	//инициализацичя плагина
+
+//
+//	$.fn._ = function (options) {
+//
+//		console.log("Initialize me");
+//
+//	}
+
+
+
+
+
+
+})(jQuery);
+
+
+
+var myHandler = function (){
+	var i = 0;
+	var counter = function() {
+		return i++;
+	}
+
+	return counter;
+
+}
+
